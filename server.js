b4d4bf8651870653b8eb7c27c3ad9b110bfcb3f7
dejandoder <dejandoder96@gmail.com>
2020-02-08 18:33:03 +0100

    heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularklinickicentar'));
+app.use(express.static(__dirname + '/IsaFrontEnd'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/angularklinickicentar/index.html'));
+res.sendFile(path.join(__dirname+'/IsaFrontEnd/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
